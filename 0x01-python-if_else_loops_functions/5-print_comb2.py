9653b5d5b308017e596201a691d4371f326e5559
josiashod <djossoujosiashod@gmail.com>
2022-06-01 13:19:03 +0100

    update task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i != 99:
-        print(f"{ i if i >= 10 else '0' + str(i) }", end=", ")
+        print("{}".format(i if i >= 10 else '0' + str(i)), end=", ")
     else:
         print(i)
