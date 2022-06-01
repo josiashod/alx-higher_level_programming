6ac2182c215809c31d2213b6056b84852dd00d8e
josiashod <djossoujosiashod@gmail.com>
2022-06-01 13:48:58 +0100

    Task 7 and Task 8

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+def islower(c):
+    _ord = ord(c)
+    return _ord >= 97 and _ord <= 122
+    
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for i in range(0, len(str)):
+        _ord = ord(str[i])
+        if _ord >= 97 and _ord <= 122:
+            str = str[:i] + chr(_ord - 32) + str[(i + 1):]
+    print("{}".format(str))
+    
