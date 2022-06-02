8a843ed5cf7c8a907c79a3e7b24201f31efdbe75
josiashod <djossoujosiashod@gmail.com>
2022-06-02 13:12:28 +0100

    Task 2 (update code)

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     from sys import argv
     _len = len(argv)
-    print("{} arguments:".format(_len - 1))
+    print("{} arguments{}".format(_len - 1, ':' if _len > 1 else '.'))
     for i in range(1, _len):
         print("{}: {}".format(i, argv[i]))
