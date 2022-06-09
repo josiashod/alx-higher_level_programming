789e8ef8c08235405b9f78f92bde764ed105f771
josiashod <djossoujosiashod@gmail.com>
2022-06-09 11:21:23 +0100

    Task 10 update code

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     """Function that returns a key with the biggest integer value."""
 
     if a_dictionary is None:
-        return ("None")
+        return None
     _max = max(a_dictionary.values())
     _max = [k for k, v in a_dictionary.items() if v == _max]
     return _max[0]
