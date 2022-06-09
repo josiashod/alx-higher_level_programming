0fd22b3e2064bde4b563036b3f9ec419007f1f1c
josiashod <djossoujosiashod@gmail.com>
2022-06-09 10:31:12 +0100

    Task 3

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """Function that that returns a
+    set of common elements in two sets.
+    """
+
+    return list(filter(lambda x: x in set_2, set_1))
