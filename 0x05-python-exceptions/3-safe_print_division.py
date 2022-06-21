8f36a971f131c7e9872a9e6720985c32e1614ef9
josiashod <djossoujosiashod@gmail.com>
2022-06-21 13:27:23 +0100

    Task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-def safe_print_division(value):
+def safe_print_division(a, b):
     """Prints an integer with "{:d}".format()."""
     try:
-        print("{:d}".format(value))
-        return True
-    except (ValueError, TypeError):
-        return (False)
+        result = a / b
+    except (ZeroDivisionError):
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return (result)
