f834a3d7fd9f08c9a23a1c1d178fb8f7f0fe842b
josiashod <djossoujosiashod@gmail.com>
2022-06-21 12:47:53 +0100

    Task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    """Prints x elements of a list."""
+
+    nb_print = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            nb_print += 1
+        except:
+            continue
+    print()
+    return (nb_print)
