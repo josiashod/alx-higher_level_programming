e716e7b76c64991503e4a62de0e036b44d0872c3
josiashod <djossoujosiashod@gmail.com>
2022-06-22 13:23:56 +0100

    Task 4 update

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -17,10 +17,6 @@ class Square():
         Args:
             size (int): The size of the square
         """
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
 
     @property
