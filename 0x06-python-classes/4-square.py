7c9f159b5bea87ee7a5a01aa7ddcd4cce47cb8d7
josiashod <djossoujosiashod@gmail.com>
2022-06-22 13:22:35 +0100

    Task 3 pdate and Task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -23,6 +23,18 @@ class Square():
             raise ValueError("size must be >= 0")
         self.__size = size
 
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
     def area(self):
         """Give the current square area"""
-        return (self.__size ** 2)
+        return (self.__size**2)
