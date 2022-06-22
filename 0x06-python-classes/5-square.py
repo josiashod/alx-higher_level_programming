2fac20e3938848b87c1cd8871036a382a9867012
josiashod <djossoujosiashod@gmail.com>
2022-06-22 13:34:28 +0100

    Task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Defines class Square
+"""
+
+
+class Square():
+    """Square class
+
+    Args:
+        size: The size of the square
+
+    """
+
+    def __init__(self, size=0) -> None:
+        """Init of an instance of squere.
+
+        Args:
+            size (int): The size of the square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Give the current square area"""
+        return (self.__size**2)
+
+    def my_print(self):
+        """Prints in stdout the square with the character #"""
+
+        _print = ""
+        for i in range(self.__size):
+            _print += "#" * self.__size
+            if i != (self.__size - 1):
+                _print += "\n"
+        print(_print)
