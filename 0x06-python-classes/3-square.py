8206ced251abb66978c4b612816a49f348a6d04b
josiashod <djossoujosiashod@gmail.com>
2022-06-22 12:20:17 +0100

    Task 3 update code

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -21,10 +21,8 @@ class Square():
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
-    @classmethod
     def area(self):
         """Give the current square area"""
         return (self.__size**2)
