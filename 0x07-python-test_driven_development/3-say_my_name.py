a4e66823da739f4ab9aed140f3525127641752a2
josiashod <djossoujosiashod@gmail.com>
2022-06-26 22:05:44 +0100

    Task 2

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This is the integer addtion module.
+>>> say_my_name(12, "Billy")
+Traceback (most recent call last):
+    ...
+TypeError: first_name must be a string"""
+
+
+def say_my_name(first_name, last_name=""):
+    """function that prints My name is <first name> <last name>
+    >>> say_my_name("Josh")
+     My name is Josh"""
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
