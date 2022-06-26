37c5cd5b7cda900440adf24e481db08a99b07c87
josiashod <djossoujosiashod@gmail.com>
2022-06-26 22:27:34 +0100

    Print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This is the integer addtion module.
+>>> print_square("Billy")
+Traceback (most recent call last):
+    ...
+TypeError: size must be an integer"""
+
+
+def print_square(size):
+    """function that prints a square with the character #
+    >>> print_square(1)
+    #"""
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    _print = ""
+    for i in range(size):
+        _print += "#" * size
+        if i != (size - 1):
+            _print += "\n"
+    print(_print)
