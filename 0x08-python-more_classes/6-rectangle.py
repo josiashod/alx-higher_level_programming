45f5a20540373183b7b94a3aa5880fd838903cdd
josiashod <djossoujosiashod@gmail.com>
2022-06-27 16:04:44 +0100

    Update perimeter function

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -54,7 +54,7 @@ class Rectangle():
 
         if (self.__width == 0) or (self.__height == 0):
             return (0)
-        return ((self.__width + self.__height) * 2)
+        return return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self) -> str:
         """Print the rectangle with the character #"""
