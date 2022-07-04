fff8f6f117786e577210dc6f617213f976c01d75
josiashod <djossoujosiashod@gmail.com>
2022-07-04 22:00:08 +0100

    Task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""MyList module"""
+
+class MyList(list):
+
+    def print_sorted(self):
+        """prints the list, but sorted (ascending sort)"""
+
+        print(sorted(self))
