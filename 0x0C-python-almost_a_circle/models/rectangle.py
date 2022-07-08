81dbac64c5e557564cf61b04965622bc008cfdd0
josiashod <djossoujosiashod@gmail.com>
2022-07-08 23:27:58 +0100

    Making all task on Rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,139 @@
+#!/usr/bin/python3
+""" Module that contains class Rectangle,
+inheritance of class Base
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """This class represent a Rectangle
+
+    Attributes:
+        __width (int): The width of the rectangle.
+        __height (int): The height of the rectangle.
+        __x (int): The x value.
+        __y (int): The y value.
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        The constructor for Rectangle class.
+
+        Parameters:
+           width (int): The width of the rectangle.
+           height (int): The height of the rectangle.
+           x (int): The x value.
+           y (int): The y value.
+           id (int): the id of the object created.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """The getter of the attribute __width"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """The getter of the attribute __height"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    @property
+    def x(self):
+        """The getter of the attribute __x"""
+        return (self.__x)
+
+    @x.setter
+    def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+
+        self.__x = value
+
+    @property
+    def y(self):
+        """The getter of the attribute __y"""
+        return (self.__y)
+
+    @y.setter
+    def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+
+        self.__y = value
+
+    def area(self):
+        """Return the area of the rectangle"""
+
+        return (self.__width * self.__height)
+
+    def display(self):
+        """ Display the rectangle """
+
+        _print = ""
+        if self.__height > 0 and self.__width > 0:
+            _print += "\n" * self.__y
+            for i in range(self.__height):
+                _print += " " * self.__x
+                _print += "#" * self.__width
+                if i != (self.__height - 1):
+                    _print += "\n"
+        print(_print)
+
+    def update(self, *args, **kwargs):
+        """ Update rectangle object with an array of argument
+
+        Parameters:
+            *args (list): list of arguments
+                1st argument should be the id attribute
+                2nd argument should be the width attribute
+                3rd argument should be the height attribute
+                4th argument should be the x attribute
+                5th argument should be the y attribute
+            **kwargs (dict): dictionnary of arguments
+                width (int): The width of the rectangle.
+                height (int): The height of the rectangle.
+                x (int): The x value.
+                y (int): The y value.
+                id (int): the id of the object created.
+        """
+
+        if args is not None and len(args) != 0:
+            list_attr = ['id', 'width', 'height', 'x', 'y']
+            for i in range(len(args)):
+                setattr(self, list_attr[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def __str__(self):
+        """ Return the str of the Rectangle """
+
+        return (f"[Rectangle] ({self.id}) {self.x}/{self.y} - \
+{self.width}/{self.height}")
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python3
+""" Module for test Rectangle class """
+import unittest
+from models.rectangle import Rectangle
+from models.base import Base
+
+class TestRectangle(unittest.TestCase):
+    """ Suite to test Rectangle class """
+
+    def setUp(self):
+        """ Method invoked for each test """
+        Base._Base__nb_objects = 0
+        self.empty_rectangle = Rectangle(0, 0)
+        self.rectangle = Rectangle(4, 2)
+
+    def test_new_rectangle(self):
+        """ Test new rectangle """
+        new = Rectangle(1, 1)
+        self.assertEqual(new.width, 1)
+        self.assertEqual(new.height, 1)
+        self.assertEqual(new.x, 0)
+        self.assertEqual(new.y, 0)
+        self.assertEqual(new.id, 3)
+
+    def test_new_rectangle_2(self):
+        """ Test new rectangle with all attrs """
+        new = Rectangle(2, 3, 5, 5, 4)
+        self.assertEqual(new.width, 2)
+        self.assertEqual(new.height, 3)
+        self.assertEqual(new.x, 5)
+        self.assertEqual(new.y, 5)
+        self.assertEqual(new.id, 4)
+
+    def test_new_rectangles(self):
+        """ Test new rectangles """
+        new = Rectangle(1, 1)
+        new2 = Rectangle(1, 1)
+        self.assertEqual(False, new is new2)
+        self.assertEqual(False, new.id == new2.id)
+
+    def test_is_Base_instance(self):
+        """ Test Rectangle is a Base instance """
+        new = Rectangle(1, 1)
+        self.assertEqual(True, isinstance(new, Base))
+
+    def test_incorrect_amount_attrs(self):
+        """ Test error raise with 1 arg passed """
+        with self.assertRaises(TypeError):
+            new = Rectangle(1)
+
+    def test_incorrect_amount_attrs_1(self):
+        """ Test error raised with no args passed """
+        with self.assertRaises(TypeError):
+            new = Rectangle()
+
+    def test_access_private_attrs(self):
+        """ Trying to access to a private attribute """
+        new = Rectangle(1, 1)
+        with self.assertRaises(AttributeError):
+            new.__width
+
+    def test_access_private_attrs_2(self):
+        """ Trying to access to a private attribute """
+        new = Rectangle(1, 1)
+        with self.assertRaises(AttributeError):
+            new.__height
+
+    def test_access_private_attrs_3(self):
+        """ Trying to access to a private attribute """
+        new = Rectangle(1, 1)
+        with self.assertRaises(AttributeError):
+            new.__x
+
+    def test_access_private_attrs_4(self):
+        """ Trying to access to a private attribute """
+        new = Rectangle(1, 1)
+        with self.assertRaises(AttributeError):
+            new.__y
+
+    def test_valide_attrs(self):
+        """ Trying to pass a string value """
+        with self.assertRaises(TypeError):
+            new = Rectangle("2", 2, 2, 2, 2)
+
+    def test_valide_attrs_2(self):
+        """ Trying to pass a string value """
+        with self.assertRaises(TypeError):
+            new = Rectangle(2, "2", 2, 2, 2)
+
+    def test_valide_attrs_3(self):
+        """ Trying to pass a string value """
+        with self.assertRaises(TypeError):
+            new = Rectangle(2, 2, "2", 2, 2)
+
+    def test_valide_attrs_4(self):
+        """ Trying to pass a string value """
+        with self.assertRaises(TypeError):
+            new = Rectangle(2, 2, 2, "2", 2)
+
+    def test_value_attrs_2(self):
+        """ Trying to pass invalid values """
+        with self.assertRaises(ValueError):
+            new = Rectangle(1, 1, -1)
+
+    def test_value_attrs_3(self):
+        """ Trying to pass invalid values """
+        with self.assertRaises(ValueError):
+            new = Rectangle(1, 1, 1, -1)
+
+    def test_area(self):
+        """ Trying to validate area of the rectangle """
+
+        new = Rectangle(4, 8)
+        self.assertEqual(32, new.area())
+
+    def test_rectangle_str_empty(self):
+        """ Trying to test the output of the str of an empty rectangle """
+
+        self.assertEqual("[Rectangle] (1) 0/0 - 0/0", str(self.empty_rectangle))
+
+    def test_rectangle_str_empty(self):
+        """ Trying to test the output of the str of a rectangle """
+
+        self.assertEqual("[Rectangle] (2) 0/0 - 4/2", str(self.rectangle))
