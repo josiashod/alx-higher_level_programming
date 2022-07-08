b87216cfd8844df6b11fce23bbc18d6f4c7d7dae
josiashod <djossoujosiashod@gmail.com>
2022-07-08 23:27:24 +0100

    Update Base doc

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 """Base module"""
 
+
 class Base:
     """This class will be the “base” of all other classes in this project.
-    The goal of it is to manage id attribute in all your future classes 
-    and to avoid duplicating the same code (by extension, same bugs)"""
+    The goal of it is to manage id attribute in all your future classes
+    and to avoid duplicating the same code (by extension, same bugs)
+
+    Attributes:
+        __nb_objects (int): to know the number of object created
+        id (int): to know the number of object created
+    """
 
     __nb_objects = 0
 
