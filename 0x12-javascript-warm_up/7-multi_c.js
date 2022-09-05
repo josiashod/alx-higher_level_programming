6d63a3414a78fb63b3a4d1e2821f87422cacc074
josiashod <djossoujosiashod@gmail.com>
2022-09-05 18:00:39 +0100

    Semistandard issues

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const argv = process.argv;
-let myNumber = Number.parseInt(argv[2]);
+const myNumber = Number.parseInt(argv[2]);
 
 if (!myNumber) {
   console.log('Missing number of occurrences');
