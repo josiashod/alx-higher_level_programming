8f4cd842c722b3ae08ae47ee9c67e6fe321e3b5b
josiashod <djossoujosiashod@gmail.com>
2022-09-05 17:19:59 +0100

    Task 10 update

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 const argv = process.argv;
-const number = Number.parseInt(argv[2]);
 
-const factorial = (number) => {
-  if (!number || number === 0 || number === 1) { return (1); }
-  return (number * factorial(number - 1));
+const factorial = (n) => {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 };
 
-console.log(factorial(number));
+console.log(factorial(Number.parseInt(argv[2])));
