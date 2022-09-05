98abafdad2671b950d656c3d95d13cf300017d5a
josiashod <djossoujosiashod@gmail.com>
2022-09-05 17:26:58 +0100

    Update code

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const argv = process.argv;
 
-const factorial = (n) => {
+function factorial (n) {
   return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 };
 
-console.log(factorial(Number.parseInt(argv[2])));
+console.log(factorial(Number(argv[2])));
--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@ let argv = process.argv.slice(2);
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  argv = argv.map(Number).sort().reverse();
+  argv = argv.map(Number).sort((a, b) => a - b).reverse();
   console.log(argv[1]);
 }
