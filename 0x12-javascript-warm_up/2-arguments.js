2c3bb6481843a63be6f2925478c333a5e41ea129
josiashod <djossoujosiashod@gmail.com>
2022-09-05 16:17:25 +0100

    Update Task 2 code

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const argv = process.argv;
 if (argv.length > 0) {
-  console.log('Argument found');
+  console.log(`Argument${argv.length > 3 ? 's' : ''} found`);
 } else {
   console.log('No argument');
 }
