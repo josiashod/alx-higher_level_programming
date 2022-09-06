ce61bb131703b4a05b6f212245780195c9a1760a
josiashod <djossoujosiashod@gmail.com>
2022-09-06 16:06:11 +0100

    Task 13

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+const src1 = fs.readFileSync(process.argv[2], 'utf8');
+const src2 = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], src1 + src2);
